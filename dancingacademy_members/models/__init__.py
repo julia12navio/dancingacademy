b1528f92ed829f488afd90ecbd5837c3455d09c1
julia12navio <navioarancejulia@gmail.com>
2024-12-03 19:54:30 +0000

    Final

--- dancingacademy_members/models/__init__.py
+++ dancingacademy_members/models/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
 
 from . import member_dancer
-from . import member_management_team
 from . import member_teacher
--- dancingacademy_members/models/member_management_team.py
+++
@@ -1,4 +0,0 @@
-from odoo import models, fields
-
-class MemberManagementTeam(models.Model):
-    _inherit = 'member.management.team'
